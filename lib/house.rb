c5cb31dc46283fdcb29a1a595478c8d87fcecfd7
Colin Suckow <csuckow99@gmail.com>
2021-04-15 10:26:10 -0700

    Implement different line number output to pass second test

--- lib/house.rb
+++ lib/house.rb
@@ -1,5 +1,10 @@
 class House
     def line(line_number)
-        "This is the house that Jack built.\n"
+        case line_number
+        when 1
+            "This is the house that Jack built.\n"
+        when 2
+            "This is the malt that lay in the house that Jack built.\n"
+        end
     end
 end
